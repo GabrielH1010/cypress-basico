275c34de31925214b834fa5cb658529ee1814169
GabrielH1010 <gabrielhdeoliveira17@gmail.com>
2023-09-20 15:37:26 -0300

    Exercicio 1
    Resolução do exercicio
    
    Mova a visita à página da aplicação em teste para um bloco beforeEach()
    Logo abaixo ao teste já existente, adicione um novo teste, chamado preenche os campos obrigatórios e envia o formulário
    Tal teste deve combinar os comandos cy.get(), .type() e .click(), para digitar seus dados nos campos Nome, Sobrenome, Email e Como podemos te ajudar?, além de clicar no botão Enviar
    Depois do clique, uma mensagem de sucesso deve ser exibida (tal mensagem deve estar contida em um elemento com a classe success). Verifique que tal mensagem está visível (.should('be.visible'))
    Por fim, execute o novo teste no Test Runner.

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,9 +2,39 @@
 
 //O bloco describe define a suíte de testes
 describe('Central de Atendimento ao Cliente TAT', function() {
+    //Este é um beforeEach global, que será executado antes de cada teste neste describe
+    beforeEach(function() {
+        cy.visit('./src/index.html')
+    })
+
     //O bloco it define um caso de teste.
     it('Verificar o título da aplicação', function() {
-        cy.visit('./src/index.html') //Comando para acessar o site
-        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT') //Comando para confirmar o titulo do site
+        //Comando para confirmar o título do site
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT') 
     })
-  })
+
+    //No caso do it.only significa que você está trabalhando somente nesse teste, ou seja, não precisa mais verificar e acessar o site se ele já está acessado
+    it.only('Preencher os campos obrigatórios e enviar o formulário', function() {
+        cy.get('input[type="text"][name="firstName"]') //Comando para pegar o parametro a ser preenchido
+            .should('be.visible') //Comando para afirmar que o campo está visivel
+            .type('Gabriel') //Comando para preencher o campo
+            .should('have.value', 'Gabriel') //Comando para comparar o valor que está preenchido
+
+        cy.get('input[type="text"][name="lastName"]')
+            .should('be.visible')
+            .type('Henrique')
+            .should('have.value', 'Henrique')
+
+        cy.get('input[type="email"]')
+            .should('be.visible')
+            .type('gabrielhdeoliveira@gmail.com')
+            .should('have.value', 'gabrielhdeoliveira@gmail.com')
+
+        cy.get('textarea[name="open-text-area"]')
+            .should('be.visible')
+            .type('Preciso de ajuda para o meu plano')
+
+        cy.get('button[type="submit"]').click() //Comando para clicar no botão
+        cy.get('.success').should('be.visible') //Comando para verificar se a mensagem de sucesso foi exibida
+    })
+})
